d6aa50b36fa0c614c3c8f1174f00b7ea71b5c89c
Hongyi Li <11414010@zju.edu.cn>
2020-07-31 00:49:20 +0800

    Site updated: 2020-07-31 00:49:19

--- js/counttime.js
+++ js/counttime.js
@@ -12,7 +12,7 @@ function safeOpenUrl(url) {
   }
 if(cjhurl == 'microbiomecat.gitee.io'){}
 else{if(lang == 'zh'){
-	layer.confirm('检测到您使用中文但访问本站境外服务器，建议访问速度快的 Gitee 备用站', {
+	layer.confirm('检测到您使用中文但访问本站境外服务器，建议访问速度更快的 Gitee 备用站', {
 		title:"等待网页加载……",
   btn: ['Gitee备用站'] ,//按钮
   shade: 0
@@ -21,7 +21,7 @@ else{if(lang == 'zh'){
 })
 }}
    // });
-   
+
 $(document).ready(function () {
 var $runtimeCount = $('#webinfo-runtime-count')
     var startDate = $runtimeCount.attr('start_date')
@@ -38,7 +38,7 @@ var $runtimeCount = $('#webinfo-runtime-count')
     interval = setInterval(showDateTime, 10000)
     if(lang == 'zh'){
     if(cjhurl == 'microbiomecat.xyz'){}
-else if(cjhurl == 'microbiomecat.gitee.io'){$(".post").prepend('<div class="post-excerpt"><div class="note-plugin success no-icon"><p><strong>提示</strong><br>检测到您正通过本站Gitee服务器进行访问。<br>建议您先在此浏览，<strong>如要收藏或分享本页，请使用本站 <a href="'+"https://microbiomecat.xyz"+pthurl+'" target="_blank" rel="noopener">Github服务器</a></strong></p></div></div>');}
+else if(cjhurl == 'microbiomecat.gitee.io'){$(".post").prepend('<div class="post-excerpt"><div class="note-plugin success no-icon"><p><strong>提示</strong><br>检测到您正通过本站Gitee服务器进行访问，此网站为国内备用网站。<br>建议您先在此浏览，<strong>如要收藏或分享本页，请使用本站 <a href="'+"https://microbiomecat.xyz"+pthurl+'" target="_blank" rel="noopener">Github服务器</a></strong></p></div></div>');}
 else if(cjhurl == 'microbiomecat.github.io'){$(".post").prepend('<div class="post-excerpt"><div class="note-plugin success no-icon"><p><strong>提示</strong><br>检测到您正通过本站Github服务器进行访问，此服务器在国内使用并不流程。如果宕机可访问Gitee服务器，<a href="'+"https://microbiomecat.gitee.io"+pthurl+'" target="_blank" rel="noopener">Gitee服务器</a><br>如要收藏本页，请前往本站独立域名网站 <a href="'+"https://microbiomecat.xyz"+pthurl+'" target="_blank" rel="noopener">Github服务器</a> 后再收藏</p></div></div>');}
 
     }
